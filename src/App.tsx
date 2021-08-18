06d1599f47ddd0779e8bc10f6f0ff79b02ab78b8
daianessousa <daisousa.c@gmail.com>
2021-08-18 08:56:55 -0300

    add page novidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import { AuthContextProvider} from './contexts/AuthContext'
 import { Home } from "./pages/Home";
+import { Novidades } from "./pages/Novidades";
 import { Register } from "./pages/Register";
 
 
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home}/>
           <Route path="/register" component={Register}/>
+          <Route path="/novidades" component={Novidades}/>
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
---
+++ src/pages/Novidades/index.tsx
@@ -0,0 +1,7 @@
+export function Novidades(){
+    return (
+        <> 
+            <h1>Novidades</h1>
+        </>
+    )
+}
