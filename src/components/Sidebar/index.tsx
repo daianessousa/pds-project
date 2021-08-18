81b6bc0dfdfe7159696d68462567539d0ebac1c3
riordansantos <riordan3607@gmail.com>
2021-08-18 10:39:34 -0300

    user info on sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,12 +1,15 @@
+import { useAuth } from '../../hooks/useAuth'
 import userPicture from '../../images/userPicture.svg'
 import './styles.scss'
 
 export function Sidebar(){
+
+    const {user} =useAuth()
     return(
         <nav className="sidebar">
-            <div>
-                <img src={userPicture} alt="Foto do Usuário"/>
-                <p>Username</p>
+            <div className="user-info">
+                <img src={user?.avatar} alt="Foto do Usuário"/>
+                <p>{user?.name}</p>
             </div>
             <div className="sidebar__buttons">
                 <button>Novidades</button>
