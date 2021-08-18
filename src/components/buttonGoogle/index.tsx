e516e6b251389bf79e629f4e873b5db85091713a
riordansantos <riordan3607@gmail.com>
2021-08-18 09:21:05 -0300

    feat(): redirect to news page on login

--- src/components/buttonGoogle/index.tsx
+++ src/components/buttonGoogle/index.tsx
@@ -1,13 +1,22 @@
 import './styles.scss'
 import { useAuth } from '../../hooks/useAuth';
 import googleIconImg from '../../images/google-icon.svg'
+import { useHistory } from 'react-router-dom'
 
 export function ButtonGoogle(){
-    const { signInWithGoogle } = useAuth()
+
+    const history = useHistory()
+    const { user, signInWithGoogle } = useAuth()
     
+    async function handleLoginWithGoogle(){
+        if(!user){
+            await signInWithGoogle()
+        }
+        history.push('/news')
+    }
     return (
         <>
-        <button onClick={signInWithGoogle} className="button-google">
+        <button onClick={handleLoginWithGoogle} className="button-google">
               <img src={googleIconImg} alt="Logo do Google" />
               Acessar com o Google
             </button>
