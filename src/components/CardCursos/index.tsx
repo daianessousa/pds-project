cb0894161eb01fac1b07a303e2bde9f2aa8376fb
Daiane Sousa <daisousa.c@gmail.com>
2021-08-31 20:08:43 -0300

    add route api cursos

--- src/components/CardCursos/index.tsx
+++ src/components/CardCursos/index.tsx
@@ -1,28 +1,28 @@
 import { BiHeart, BiChevronsRight } from "react-icons/bi"
 import './styles.scss'
 
-export function CardCursos(){
-    return(
-        <div className="content-curso">
-            {/* <img src="" alt="" /> */}
-            <div className="image">
+export function CardCursos() {
+  return (
+    <div className="content-curso">
+      {/* <img src="" alt="" /> */}
+      <div className="image">
 
-            </div>
-            <div className="content">
-              <div className="content-tittle">
-                <h3>Título em dev</h3>
-                <a href=""><BiHeart/></a>
-              </div>
-              
-              <span>Atualização em 19/07/2021</span>
-              <div className="tipo">
-                Gratuito
-              </div>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
-              <div className="more-info">
-                <a>Saiba mais <BiChevronsRight /></a>
-              </div>
-            </div>
-          </div>
-    )
+      </div>
+      <div className="content">
+        <div className="content-tittle">
+          <h3>Título em dev</h3>
+          <a href=""><BiHeart /></a>
+        </div>
+
+        <span>Atualização em 19/07/2021</span>
+        <div className="tipo">
+          Gratuito
+        </div>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
+        <div className="more-info">
+          <a>Saiba mais <BiChevronsRight /></a>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/Cursos/index.tsx
+++ src/pages/Cursos/index.tsx
@@ -1,42 +1,49 @@
+import { useEffect, useState } from 'react'
 import { Sidebar } from '../../components/Sidebar'
 import { CardCursos } from '../../components/CardCursos'
 import './styles.scss'
+import api from '../../services/api'
+
+interface CursosProps {
+  title: string,
+  description: string,
+  cover: string,
+  link: string,
+  id: number,
+}
+
 
 export function Cursos() {
+
+  const [cursos, setCursos] = useState<CursosProps[]>([]);
+
+  useEffect(() => {
+    api
+      .get<CursosProps[]>("/cursos")
+      .then((response) => setCursos(response.data))
+      .catch((err) => {
+        console.error("Ocorreu um erro: " + err);
+      });
+  }, []);
+
   return (
-      <section className="news">
-        <Sidebar />
-        <div className="container-news">
-          <div className="tittle">
-            <h1>Cursos</h1>
-          </div>
-          <div className="cards">
-            <div className="card">
-              <CardCursos />
-            </div>
-            <div className="card">
-              <CardCursos />
-            </div>
-            <div className="card">
-              <CardCursos />
-            </div>
-            <div className="card">
-              <CardCursos />
-            </div>
-            <div className="card">
-              <CardCursos />
-            </div>
-            <div className="card">
-              <CardCursos />
-            </div>
-                      
-            
-            
-            
-          </div>
-          
+    <section className="news">
+      <Sidebar />
+      <div className="container-news">
+        <div className="tittle">
+          <h1>Cursos</h1>
         </div>
-        
-      </section>
+        <div className="cards">
+          {cursos.map((curso) => (
+            <div key={curso.id} className="card">
+              <CardCursos />
+            </div>
+          ))}
+
+        </div>
+
+      </div>
+
+    </section>
   )
 }
