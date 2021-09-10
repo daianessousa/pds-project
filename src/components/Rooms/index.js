e8ae4b7ac116dfbc41a5aeda3540686aa1676964
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-09-10 11:11:49 -0300

    refactoring styles

--- src/components/Rooms/index.js
+++ src/components/Rooms/index.js
@@ -27,12 +27,14 @@ export function Rooms({room}){
               <div className="content">
                 <div className="footer-buttons">
                     <p>{room.title}</p>
-                    <a href={`/rooms/${room.id}`}>
-                        <button>Entrar na sala</button>
-                    </a>
+                    
                     {user.id === room.authorId ? <div>{
                     <button onClick={handleJoinRoom}>Editar Sala</button>    
                     }</div> : ''}
+                    
+                    <a href={`/rooms/${room.id}`}>
+                        <button>Entrar na sala</button>
+                    </a>
                 </div>
             </div>
         </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { ButtonGoogle } from '../../components/ButtonGoogle'
+import logoHelpJr from "../../images/computador.png"
 import './styles.scss'
 
 export function Home() {
@@ -8,7 +9,10 @@ export function Home() {
          </div>
          <div className="homeAcess">
             <div className="container">
-               <h1 className="logo">HelpJr</h1>
+               <div className="logo-title">
+                  <img src={logoHelpJr} alt="logo" />
+                  <h1 className="logo">HelpJr</h1>
+               </div>
                <h1 className="texto">Iniciando como Dev? Temos as melhores dicas aqui</h1>
                <p className="separator"></p>
                <ButtonGoogle />
